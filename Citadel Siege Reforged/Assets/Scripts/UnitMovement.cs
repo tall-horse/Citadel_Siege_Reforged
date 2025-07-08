f1f36414279354aa79f62164871bef974638348a
tall-horse <dima.gamedev@pm.me>
2025-07-08 16:03:32 +0200

    disabling physics for dead units

--- Citadel Siege Reforged/Assets/Scripts/UnitMovement.cs
+++ Citadel Siege Reforged/Assets/Scripts/UnitMovement.cs
@@ -6,10 +6,18 @@ public class UnitMovement : MonoBehaviour
     private Rigidbody rb;
     private Unit unit;
     private float indirectMultipliyer = 10f;
+    private Health health;
+    private Collider collider;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         unit = GetComponent<Unit>();
+        health = GetComponent<Health>();
+        collider = GetComponent<Collider>();
+    }
+    private void Start()
+    {
+        health.OnDead += DisablePhysics;
     }
 
     // Update is called once per frame
@@ -17,5 +25,12 @@ public class UnitMovement : MonoBehaviour
     {
         if (unit.UnitState.currentState != UnitState.UNITSTATE.WALK) return;
         rb.linearVelocity = transform.forward * speed * indirectMultipliyer * Time.deltaTime;
+    }
+
+    private void DisablePhysics()
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        collider.enabled = false;
     }
 }
